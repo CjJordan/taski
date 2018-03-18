0fb5747e0274f21678df3198eaa317ee3dff0849
Cj Jordan <cj.jordan.tech@gmail.com>
2018-03-18 07:16:23 -0500

    adds todos

--- client/src/TodoContainer.js
+++ client/src/TodoContainer.js
@@ -4,6 +4,18 @@ import TagPanel from './components/TagPanel';
 import TodoPanel from './components/TodoPanel';
 import MenuPanel from './components/MenuPanel';
 
+const tasks =  [
+  {title:"spin class" , tags:["social", "fitness"], complete: false},
+  {title:"julia's party" , tags:["social"], complete: false},
+  {title:"spanish meetup" , tags:["social", "spanish"], complete: false},
+  {title:"duolingo" , tags:["spanish"], complete: false},
+  {title:"lift weights" , tags:["fitness"], complete: false},
+  {title:"laundry" , tags:["housework"], complete: false},
+  {title:"dishes" , tags:["housework"], complete: false},
+  {title:"vacuum" , tags:["housework"], complete: false},
+  {title:"walk 606 with Shamika" , tags:["fitness", "social"], complete: false}
+];
+
 class TodoContainer extends Component {
     state = {
       tags: ['spanish', 'housework', 'social', 'fitness', 'web dev']
